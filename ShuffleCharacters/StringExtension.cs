d31dac430b95687afef593169089aa4dbd7ae014
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-07 08:41:23 +0000

    Upload New File

---
+++ ShuffleCharacters/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ShuffleCharacters
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Shuffles characters in source string according some rule.
+        /// </summary>
+        /// <param name="source">The source string.</param>
+        /// <param name="count">The count of iterations.</param>
+        /// <returns>Result string.</returns>
+        /// <exception cref="ArgumentException">Source string is null or empty or white spaces.</exception>
+        /// <exception cref="ArgumentException">Count of iterations less than 0.</exception>
+        public static string ShuffleChars(string source, int count)
+        {
+            throw  new NotImplementedException();
+        }
+    }
+}
