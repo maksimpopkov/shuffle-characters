c2c394432ca2c2bf0935d706bd43047a45e74106
Vitali Novik <Vitali_Novik@epam.com>
2020-08-23 00:06:07 +0300

    Added TestFixture attribute

--- ShuffleCharacters.Tests/StringExtensionTests.cs
+++ ShuffleCharacters.Tests/StringExtensionTests.cs
@@ -7,6 +7,7 @@ using static ShuffleCharacters.StringExtension;
 
 namespace ShuffleCharacters.Tests
 {
+    [TestFixture]
     public class StringExtensionTests
     {
         private static IEnumerable<TestCaseData> TestCases
