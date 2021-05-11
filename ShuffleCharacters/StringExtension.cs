19be0024a01a90cb302b5135e2f2bbeaef7a4775
Alexander Rykov <154324-alexander-rykov@users.noreply.gitlab.com>
2021-05-11 15:23:27 +0000

    Add "is" word.

--- ShuffleCharacters/StringExtension.cs
+++ ShuffleCharacters/StringExtension.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ShuffleCharacters
 {
@@ -11,7 +11,7 @@ namespace ShuffleCharacters
         /// <param name="count">The count of iterations.</param>
         /// <returns>Result string.</returns>
         /// <exception cref="ArgumentException">Source string is null or empty or white spaces.</exception>
-        /// <exception cref="ArgumentException">Count of iterations less than 0.</exception>
+        /// <exception cref="ArgumentException">Count of iterations is less than 0.</exception>
         public static string ShuffleChars(string source, int count)
         {
             throw new NotImplementedException();
